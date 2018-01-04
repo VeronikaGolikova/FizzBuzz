cebb12aa826df525202d7e39e5fc929dd46daecc
Veronika_Golikova <golikova.veronika.igorevna@mail.ru>
2018-01-04 16:45:13 +0400

    Удалена лишняя переменная "а".

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    int a = 1;
 	for (int i=1; i<101; i++) {
-	    if (a % 15 == 0) {
+	    if (i % 15 == 0) {
 	        System.out.println("FizzBuzz");
 	    }
-	    else if (a % 3 == 0) {
+	    else if (i % 3 == 0) {
             System.out.println("Fizz");
         }
-        else if (a % 5 == 0) {
+        else if (i % 5 == 0) {
             System.out.println("Buzz");
         }
         else {
-            System.out.println(a);
+            System.out.println(i);
         }
-	    a++;
         }
     }
 }
